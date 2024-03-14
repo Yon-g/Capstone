f42e672221ab51bfd3bdabdf44d33aade8c819aa
SepYoKim6745 <kys1479@koreatech.ac.kr>
2024-03-14 22:40:46 +0900

    SLAM MAP 레이아웃 구성 완료

--- Front/components/MapComponet/index.js
+++ Front/components/MapComponet/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import { StyledMapContainer } from "./styles";
 
 const MapComponet = ({ turtlebots }) => {
-  return <StyledMapContainer></StyledMapContainer>;
+  return (
+    <StyledMapContainer>
+      <img src="./images/map_rotate.png" alt="SLAM MAP"></img>
+    </StyledMapContainer>
+  );
 };
 
 export default MapComponet;
--- Front/components/MapComponet/styles.js
+++ Front/components/MapComponet/styles.js
@@ -4,4 +4,10 @@ export const StyledMapContainer = styled.div`
   width: 100%;
   height: 100%;
   position: relative;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: container;
+  }
 `;
