80143fa403fe1ac43c2ce84d161e1fdb326f21a1
jm-kor-00 <tomy000120@naver.com>
2024-03-23 00:18:48 +0900

    app.js annotation

--- Back/chair/src/App.js
+++ Back/chair/src/App.js
@@ -5,6 +5,11 @@ const App = () => {
   const [Y, setY] = useState(0);
   const [ORT, setORT] = useState(0);
 
+
+  // interval(=1.000초) 마다 플라스크 웹서버에 라우팅 함수를 호출해서
+  // 실시간으로 변경되는 터틀봇 좌표값을 state X, Y, ORT에 저장
+  // 웹서버에서 넘어오는 json파일은 console로 확인바람
+
   useEffect(() => {
     const interval = setInterval(() => {
       fetch('http://localhost:5000/socket_Pos')
