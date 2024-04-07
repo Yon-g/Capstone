410f70b96f952aab12c6a44f1f111a57dc15ed03
SepYoKim6745 <kys1479@koreatech.ac.kr>
2024-04-08 00:24:28 +0900

    작업 상태에 따른 활성화 비활성화버튼 코드 작성 완료

--- Front/components/StyledLayout/index.js
+++ Front/components/StyledLayout/index.js
@@ -7,8 +7,21 @@ import MapComponet from "../MapComponet";
 
 export default function LayoutComponet() {
   const [turtlebots, setTurtlebots] = useState([]);
+  const [order, setOrder] = useState(0);
 
+  //작업 명령 상태 정보를 비동기적으로 가져오는 함수
   useEffect(() => {
+    const fetchTurtlebotOrder = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/socket_order");
+        const data = await response.json();
+        setOrder(data.order);
+        console.log(data.order);
+      } catch (error) {
+        console.error("TurtleBot position fetching error: ", error);
+      }
+    };
+
     // Turtlebot의 위치 정보를 비동기적으로 가져오는 함수
     const fetchTurtlebotPositions = async () => {
       try {
@@ -28,9 +41,13 @@ export default function LayoutComponet() {
     };
 
     // 실제 데이터로 마커 위치 업데이트
-    const intervalId = setInterval(fetchTurtlebotPositions, 100);
+    const orderIntervalId = setInterval(fetchTurtlebotOrder, 1000);
+    const positionIntervalId = setInterval(fetchTurtlebotPositions, 100);
 
-    return () => clearInterval(intervalId);
+    return () => {
+      clearInterval(orderIntervalId);
+      clearInterval(positionIntervalId);
+    };
   }, []);
 
   return (
@@ -38,7 +55,7 @@ export default function LayoutComponet() {
       <Header></Header>
       <Content></Content>
       <MapComponet turtlebots={turtlebots}></MapComponet>
-      <FooterComponents></FooterComponents>
+      <FooterComponents order={order}></FooterComponents>
     </StyledLayout>
   );
 }
