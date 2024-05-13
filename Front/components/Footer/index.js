92bd947547ce5212157672e7d3b168d6135968ea
songseungheon <gjs0507@gmail.com>
2024-05-13 23:49:57 +0900

    서버 url 수정 완료

--- Front/components/Footer/index.js
+++ Front/components/Footer/index.js
@@ -28,7 +28,7 @@ export default function FooterComponents({ order }) {
     setLoading(true);
     try {
       // 여기에 서버로 데이터를 전송하는 코드를 작성
-      const response = await fetch("http://localhost:5000/user_order", {
+      const response = await fetch("http://192.168.0.130:5000/user_order", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -55,7 +55,7 @@ export default function FooterComponents({ order }) {
       const stopSignal = JSON.stringify({ option: -1 }); //취소신호를 -1로 함 
       console.log("서버로 보내는 데이터:", stopSignal); // 콘솔에 서버로 보낼 데이터 출력 확인용
 
-      const response = await fetch("http://localhost:5000/user_order", { //이 주소가 맞는지 확인 필요할 듯
+      const response = await fetch("http://192.168.0.130:5000/user_order", { //이 주소가 맞는지 확인 필요할 듯
         method: "POST",
         headers: {
           "Content-Type": "application/json",
